e4c07ac2f67411179804869fa9b6902b4017a165
Nikos Kalomiris <nikoskalomiris@gmail.com>
2019-06-15 23:13:31 +0300

    Update Android.mk

--- init/Android.mk
+++ init/Android.mk
@@ -18,15 +18,10 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_C_INCLUDES := \
-    system/core/base/include \
-    system/core/init
+LOCAL_C_INCLUDES := system/core/init
 LOCAL_MODULE := libinit_ugg
 LOCAL_MODULE_TAGS := optional
+LOCAL_CFLAGS := -Wall -DANDROID_TARGET=\"$(TARGET_BOARD_PLATFORM)\"
 LOCAL_SRC_FILES := init_ugg.cpp
-
-ifneq ($(TARGET_LIBINIT_MSM8937_DEFINES_FILE),)
-    LOCAL_SRC_FILES += ../../../../$(TARGET_LIBINIT_MSM8937_DEFINES_FILE)
-endif
 
 include $(BUILD_STATIC_LIBRARY)
