454a9e70f9532a249fba5e40a05143edbe1a6872
Nikos Kalomiris <nikoskalomiris@gmail.com>
2019-06-15 23:11:16 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -144,7 +144,6 @@ TARGET_ANDROID_FILESYSTEM_CONFIG_H := $(LOCAL_PATH)/android_filesystem_config.h
 # Init
 TARGET_INIT_VENDOR_LIB := libinit_ugg
 TARGET_RECOVERY_DEVICE_MODULES := libinit_ugg
-TARGET_LIBINIT_MSM8937_DEFINES_FILE := $(LOCAL_PATH)/init/init_ugg.cpp
 
 # Malloc
 MALLOC_SVELTE := true
