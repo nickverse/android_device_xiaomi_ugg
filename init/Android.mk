3852ecf5878c72d09e368347884e4a8f7fa2a88b
Nikos Kalomiris <nikoskalomiris@gmail.com>
2019-06-15 03:36:44 +0300

    Update Android.mk

--- init/Android.mk
+++ init/Android.mk
@@ -25,4 +25,8 @@ LOCAL_MODULE := libinit_ugg
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := init_ugg.cpp
 
+ifneq ($(TARGET_LIBINIT_MSM8937_DEFINES_FILE),)
+    LOCAL_SRC_FILES += ../../../../$(TARGET_LIBINIT_MSM8937_DEFINES_FILE)
+endif
+
 include $(BUILD_STATIC_LIBRARY)
