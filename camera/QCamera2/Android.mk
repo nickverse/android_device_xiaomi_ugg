96657d57ab56defe68bd88467f480c1022626f1c
Nikos Kalomiris <nikoskalomiris@gmail.com>
2019-06-18 20:18:09 +0300

    Update Android.mk

--- camera/QCamera2/Android.mk
+++ camera/QCamera2/Android.mk
@@ -67,15 +67,7 @@ ifeq ($(TARGET_USES_MEDIA_EXTENSIONS), true)
 LOCAL_CFLAGS += -DUSE_MEDIA_EXTENSIONS
 endif
 
-#USE_DISPLAY_SERVICE from Android O onwards
-#to receive vsync event from display
-ifeq ($(filter OMR1 O 8.1.0, $(PLATFORM_VERSION)), )
-USE_DISPLAY_SERVICE := true
-LOCAL_CFLAGS += -DUSE_DISPLAY_SERVICE
-LOCAL_CFLAGS += -std=c++11 -std=gnu++1y
-else
 LOCAL_CFLAGS += -std=c++11 -std=gnu++0x
-endif
 
 #HAL 1.0 Flags
 LOCAL_CFLAGS += -DDEFAULT_DENOISE_MODE_ON -DHAL3 -DQCAMERA_REDEFINE_LOG
@@ -120,14 +112,10 @@ LOCAL_C_INCLUDES += \
         $(TARGET_OUT_HEADERS)/qcom/display
 LOCAL_C_INCLUDES += \
         $(call project-path-for,qcom-display)/libqservice
-LOCAL_SHARED_LIBRARIES := liblog libhardware libutils libcutils libdl libsync
+LOCAL_SHARED_LIBRARIES := libcamera_client liblog libhardware libutils libcutils libdl libsync libgui
 LOCAL_SHARED_LIBRARIES += libmmcamera_interface libmmjpeg_interface libui libcamera_metadata
 LOCAL_SHARED_LIBRARIES += libqdMetaData libqservice libbinder
-ifeq ($(USE_DISPLAY_SERVICE),true)
-LOCAL_SHARED_LIBRARIES += android.frameworks.displayservice@1.0 libhidlbase libhidltransport
-else
-LOCAL_SHARED_LIBRARIES += libgui
-endif
+LOCAL_SHARED_LIBRARIES += libcutils libdl
 ifeq ($(TARGET_TS_MAKEUP),true)
 LOCAL_SHARED_LIBRARIES += libts_face_beautify_hal libts_detected_face_hal
 endif
